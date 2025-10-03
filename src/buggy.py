dda8102e8234593c0eea21c658d0c19afaefb076
rolandgulyas90 <rolandgulyas90@gmail.com>
2025-10-03 23:07:57 +0200

    refactor: iranyvektro terkep keszitese a mozgasi logikahoz

--- src/buggy.py
+++ src/buggy.py
@@ -3,9 +3,17 @@ class Buggy:
         self.x = x
         self.y = y
         self.direction = direction
+        self._direction_vectors = {
+            'N': (0, -1),
+            'S': (0, 1),
+        }
 
     def move(self, command: str):
-        if command == 'f' and self.direction == 'N':
-            self.y -= 1
-        elif command == 'f' and self.direction == 'S':
-            self.y += 1
+        if command == 'f':
+            # Kikeresi a jelenlegi irányhoz tartozó vektort
+            vector = self._direction_vectors.get(self.direction)
+
+            if vector:
+                dx, dy = vector
+                self.x += dx
+                self.y += dy

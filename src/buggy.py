6f6ac812b73b114e6fc1576892f1054fe187aada
rolandgulyas90 <rolandgulyas90@gmail.com>
2025-10-03 23:26:27 +0200

    refactor: Fordulási logikához szótár készítés

--- src/buggy.py
+++ src/buggy.py
@@ -3,28 +3,33 @@ class Buggy:
         self.x = x
         self.y = y
         self.direction = direction
+
         self._direction_vectors = {
-            'N': (0, -1),
-            'S': (0, 1),
-            'E': (1, 0),
-            'W': (-1, 0),
+            'N': (0, -1), 'S': (0, 1), 'E': (1, 0), 'W': (-1, 0),
+        }
+
+        # ÚJ: Szótár a balra forduláshoz
+        self._left_turn_map = {
+            'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N',
+        }
+        # ÚJ: Szótár a jobbra forduláshoz (ezt majd később használjuk)
+        self._right_turn_map = {
+            'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N',
         }
 
     def move(self, command: str):
-        vector = self._direction_vectors.get(self.direction)
-        if not vector:
-            return  # Ha ismeretlen az irány, ne csináljon semmit
+        if command == 'f' or command == 'b':
+            vector = self._direction_vectors.get(self.direction)
+            if not vector: return
+            dx, dy = vector
 
-        dx, dy = vector
+            if command == 'f':
+                self.x += dx
+                self.y += dy
+            elif command == 'b':
+                self.x -= dx
+                self.y -= dy
 
-        if command == 'f':
-            self.x += dx
-            self.y += dy
-        elif command == 'b':
-            self.x -= dx  # A vektor ellenkezőjét alkalmazzuk
-            self.y -= dy  # A vektor ellenkezőjét alkalmazzuk
+        # ÁTÍRVA: A fordulás logikája mostantól egy szótárat használ
         elif command == 'l':
-            if self.direction == 'N':
-                self.direction = 'W'
-            elif self.direction == 'W':  # <-- EZT ADD HOZZÁ
-                self.direction = 'S'  # <-- ÉS EZT
+            self.direction = self._left_turn_map.get(self.direction, self.direction)
